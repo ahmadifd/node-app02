4769f3292f0d78d52a34e9b696fda7747a467ac6
Farshid Ahmadi <ahmadi.fd@hotmail.com>
2023-11-16 10:59:00 +0330

    reqwe

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 await mongoose
   .connect(
     //"mongodb://root:XMBjo2J0QUCl9dRu@services.irn1.chabokan.net:2039/CompanyDB"
-    "mongodb+srv://ahmadifd:47mcOiksjZjyvH1Y@cluster0.jad41dh.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://ahmadifd:47mcOiksjZjyvH1Y@cluster0.jad41dh.mongodb.net/CompanyDB"
   )
   .then(async () => {
     console.log("connected");
