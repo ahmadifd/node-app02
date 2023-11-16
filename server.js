2be8e58dab3062706b3743d33c51fdceb29c84e1
Farshid Ahmadi <ahmadi.fd@hotmail.com>
2023-11-16 11:23:38 +0330

    qwe

--- server.js
+++ server.js
@@ -6,15 +6,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-// await mongoose
-//   .connect(
-//     //"mongodb://root:XMBjo2J0QUCl9dRu@services.irn1.chabokan.net:2039/CompanyDB"
-//     "mongodb+srv://ahmadifd:47mcOiksjZjyvH1Y@cluster0.jad41dh.mongodb.net/CompanyDB"
-//   )
-//   .then(async () => {
-//     console.log("connected");
-//   })
-//   .catch((err) => console.log("not connected", err));
+await mongoose
+  .connect(
+    "mongodb://root:XMBjo2J0QUCl9dRu@services.irn1.chabokan.net:2039/CompanyDB"
+    //"mongodb+srv://ahmadifd:47mcOiksjZjyvH1Y@cluster0.jad41dh.mongodb.net/CompanyDB"
+  )
+  .then(async () => {
+    console.log("connected");
+  })
+  .catch((err) => console.log("not connected", err));
 
 app.get("/api/users", (req, res) => {
   console.log("Hello Farshid");
