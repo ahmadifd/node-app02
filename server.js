466c86ef1e9977a502a777c9218cf4ef3498b77a
Farshid Ahmadi <ahmadi.fd@hotmail.com>
2023-12-03 19:29:36 +0330

    2343

--- server.js
+++ server.js
@@ -1,27 +1,37 @@
 import express from "express";
 import mongoose from "mongoose";
+import { connectDB } from "./config/dbConn.js";
 
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+connectDB();
 
 
-await mongoose  
-  .connect(
-    //"mongodb://root:XMBjo2J0QUCl9dRu@services.irn1.chabokan.net:2039/CompanyDB"eee
-    "mongodb+srv://ahmadifd:47mcOiksjZjyvH1Y@cluster0.jad41dh.mongodb.net/CompanyDB"
-  )
-  .then(async () => {
-    console.log("connected");
-  })
-  .catch((err) => console.log("not connected", err));
+mongoose.connection.once('open', () => {
+  console.log('Connected to MongoDB')
 
-app.get("/api/users", (req, res) => {
-  console.log("Hello Farshid");
-  res.send("Hello Farshid Ahmadi 1");
-});
+})
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`listen to ${PORT}`);
-});
+
+
+// const app = express();
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// await mongoose
+//   .connect(
+//     //"mongodb://root:XMBjo2J0QUCl9dRu@services.irn1.chabokan.net:2039/CompanyDB"eee
+//     "mongodb+srv://ahmadifd:47mcOiksjZjyvH1Y@cluster0.jad41dh.mongodb.net/CompanyDB"
+//   )
+//   .then(async () => {
+//     console.log("connected");
+//   })
+//   .catch((err) => console.log("not connected", err));
+
+// app.get("/api/users", (req, res) => {
+//   console.log("Hello Farshid");
+//   res.send("Hello Farshid Ahmadi 1");
+// });
+
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`listen to ${PORT}`);
+// });
