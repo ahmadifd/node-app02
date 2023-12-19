1580b067ebf9b49dd6ab559ee971f0ee386810e0
Farshid Ahmadi <ahmadi.fd@hotmail.com>
2023-12-19 19:31:02 +0330

    qwer

--- config/dbConn.js
+++ config/dbConn.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/node-app02");
+    await mongoose.connect(
+      //"mongodb://127.0.0.1:27017/node-app02"
+      "mongodb+srv://ahmadifd:47mcOiksjZjyvH1Y@cluster0.jad41dh.mongodb.net/node-app02"
+      );
   } catch (err) {
     console.log(err);
   }
