1dea55803e41e15b608b0e70a0868271ca973f45
Farshid Ahmadi <ahmadi.fd@hotmail.com>
2023-12-20 11:35:08 +0330

    ewrt

--- controllers/authController.js
+++ controllers/authController.js
@@ -27,7 +27,7 @@ const login = async (req, res) => {
       },
     },
     "sdfjhj234t2fwd0982i34rf23feoijf042SDF",
-    { expiresIn: "5s" }
+    { expiresIn: "10s" }
   );
 
   const refreshToken = jwt.sign(
@@ -129,7 +129,7 @@ const refresh = (req, res) => {
           },
         },
         "sdfjhj234t2fwd0982i34rf23feoijf042SDF",
-        { expiresIn: "5s" }
+        { expiresIn: "10s" }
       );
 
       return controller.response({
